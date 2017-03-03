5dce3a6313651fe321cd84839b867340942a07af
Tadej Novak <tadej@tano.si>
2017-03-03 22:29:23 +0100

    Add basic testing and coverage support

---
+++ joda_core/files/tests.py
@@ -0,0 +1,21 @@
+import os
+
+from django.test import TestCase
+
+import joda_core.files.utils as utils
+
+
+class FileUtilsTestCase(TestCase):
+    def test_file_properties(self):
+        """Test file MD5 and size"""
+        sample_pdf = 'sample.pdf'
+        os.environ['FILES_UPLOAD_PATH'] = 'test_data'
+
+        path = utils.upload_path()
+        self.assertEqual(path, 'test_data')
+        self.assertEqual(utils.get_size(sample_pdf), 7104)
+        with open(os.path.join(path, sample_pdf), 'rb') as f:
+            self.assertEqual(utils.get_md5(f), '84ccae833dd13ee79642d0095440da9a')
+
+        os.environ['FILES_UPLOAD_PATH'] = 'jure'
+        self.assertEqual(utils.upload_path(), 'test_data')
