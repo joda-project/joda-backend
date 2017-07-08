a66c0da9bffb7dc9fd41963f6fcb7fb36797dff8
Tadej Novak <tadej@tano.si>
2017-07-08 19:30:41 +0200

    Update packages

--- joda/settings.py
+++ joda/settings.py
@@ -87,7 +87,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_METADATA_CLASS': 'rest_framework_json_api.metadata.JSONAPIMetadata',
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'oauth2_provider.ext.rest_framework.OAuth2Authentication',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
         'rest_framework.authentication.SessionAuthentication',
     )
 }
